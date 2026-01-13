431e616656f34ed01142609aa09386b8c80ebc98
Serghy Castillo <b1uefeet.dev@gmail.com>
2026-01-13 18:15:02 -0500

    Proyecto Base + Actualizacion de escena de pruebas para movimiento de fotos 360

---
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+
+namespace StarterAssets
+{
+    public class StarterAssetsInputs : MonoBehaviour
+    {
+        [Header("Character Input Values")]
+        public Vector2 move;
+        public Vector2 look;
+        public bool jump;
+        public bool sprint;
+        public bool interact;
+        public bool returnMenu;
+
+        [Header("Movement Settings")]
+        public bool analogMovement;
+
+        [Header("Mouse Cursor Settings")]
+        public bool cursorLocked = true;
+        public bool cursorInputForLook = true;
+
+#if ENABLE_INPUT_SYSTEM
+        public void OnMove(InputValue value)
+        {
+            MoveInput(value.Get<Vector2>());
+        }
+
+        public void OnLook(InputValue value)
+        {
+            if (cursorInputForLook)
+            {
+                LookInput(value.Get<Vector2>());
+            }
+        }
+
+        public void OnJump(InputValue value)
+        {
+            JumpInput(value.isPressed);
+        }
+
+        public void OnSprint(InputValue value)
+        {
+            SprintInput(value.isPressed);
+        }
+
+        public void OnInteract(InputValue value)
+        {
+            InteractInput(value.isPressed);
+            Debug.Log("OnInteract called");
+        }
+
+        public void OnReturnMenu(InputValue value)
+        {
+            ReturnMenuInput(value.isPressed);
+            Debug.Log("OnReturnMenu called");
+        }
+#endif
+
+        public void MoveInput(Vector2 newMoveDirection)
+        {
+            move = newMoveDirection;
+        }
+
+        public void LookInput(Vector2 newLookDirection)
+        {
+            look = newLookDirection;
+        }
+
+        public void JumpInput(bool newJumpState)
+        {
+            jump = newJumpState;
+        }
+
+        public void SprintInput(bool newSprintState)
+        {
+            sprint = newSprintState;
+        }
+
+        public void InteractInput(bool newInteractState)
+        {
+            interact = newInteractState;
+            Debug.Log("Interact input: " + interact);
+        }
+
+        public void ReturnMenuInput(bool newReturnMenuState)
+        {
+            returnMenu = newReturnMenuState;
+        }
+
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            SetCursorState(cursorLocked);
+        }
+
+        public void SetCursorState(bool newState)
+        {
+            Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+            Cursor.visible = !newState;
+        }
+
+        public void SetCursorForUI(bool uiOpen)
+        {
+            cursorLocked = !uiOpen;
+            cursorInputForLook = !uiOpen;
+            SetCursorState(cursorLocked);
+        }
+    }
+}
